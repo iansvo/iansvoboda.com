114c7f5d6fb3e93b3f634e936c9d2cb5c2817e21
Ian Svoboda <ian@iansvoboda.com>
2019-05-06 16:04:48 -0400

    Update work-together CTA link

--- wp-content/themes/iansvo-2019/parts/work-together.php
+++ wp-content/themes/iansvo-2019/parts/work-together.php
@@ -22,6 +22,6 @@
   	</header>
   	<div class="c-text c-text--center u-text--center">
 		<p>Do you have a beautiful design concept or mockup you want brought to life? I can take your finished design (PSD/AI/Sketch) and turn it into a beautiful, responsive website, implement that slick UI that finally got approved, and more. Did you hire someone else and find out they didn’t have the experience to build the site you’ve designed? I can help.</p>
-		<a href="#" class="c-button">Hire Me</a>
+		<a href="/contact" class="c-button">Hire Me</a>
   	</div>
 </aside>
